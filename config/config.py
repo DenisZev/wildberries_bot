93f1a2c7c62c0383ab27290f2c8e66deb60c8368
DessaZev <43928077+DenisZev@users.noreply.github.com>
2025-03-02 02:27:49 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 # config/config.py
-BOT_KEY = "7210035997:AAETKsY0RI_BnKZNngKY-raSz7mrVRkDmlo"
-CHAT_ID = "-1002183064388"
-API_KEY = "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjQxMjE3djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc1MDQzOTY1OCwiaWQiOiIwMTkzZTI3Yy1lY2YyLTcxOTEtYTQ5ZC0wZGI4YmIyMzgzMWYiLCJpaWQiOjQ2NjQxOTAyLCJvaWQiOjQyMDA3NDksInMiOjc5MzQsInNpZCI6IjBjNzMyYjJiLTQ1ZjUtNGJkZS04MzM5LTY2ODYzNTdiMjM3NSIsInQiOmZhbHNlLCJ1aWQiOjQ2NjQxOTAyfQ.YX27cJbfLwWnz6ckWtLuK7z0ZgX0Miv_RnVhUVlzIJ46Q8UmHnqmE2YcGOD06DUR2ELmuWYI5wVrFl9kp-5cEw"
+BOT_KEY = ""
+CHAT_ID = ""
+API_KEY = ""
 BASE_URL = "https://marketplace-api.wildberries.ru/api/v3"
 CONTENT_URL = "https://content-api.wildberries.ru/content/v2/get/cards"
 CHECK_INTERVAL = 120  # Интервал проверки в секундах (изменил с 360 на 120, как в scheduler)
